0ac96a581fb142c14e3746a988e376b14acb55d8
Dragonmiir <voscar.souza@gmail.com>
2023-09-19 20:30:52 -0300

    Estudo sobre programação com jogo pong

--- Pong.py
+++ Pong.py
@@ -26,6 +26,54 @@ barra_b.shapesize(stretch_wid=5, stretch_len=1)
 barra_b.penup()
 barra_b.goto(350, 0)
 
-# Mina game loop
+# Bola
+
+bola = turtle.Turtle()
+bola.speed()
+bola.shape("square")
+bola.color("white")
+bola.penup()
+bola.goto(0, 0)
+
+# Funções para fazer com que as barras se movam
+
+#Barra A
+def barra_a_cima():
+    y = barra_a.ycor()
+    y += 20
+    barra_a.sety(y)
+
+def barra_a_baixo():
+    y = barra_a.ycor()
+    y -= 20
+    barra_a.sety(y)
+
+#Barra B
+
+def barra_b_cima():
+    y = barra_b.ycor()
+    y += 20
+    barra_b.sety(y)
+
+def barra_b_baixo():
+    y = barra_b.ycor()
+    y -= 20
+    barra_b.sety(y)
+
+# Definição das teclas
+
+tela.listen()
+tela.onkeypress(barra_a_cima, "w")
+
+tela.listen()
+tela.onkeypress(barra_a_baixo, "s")
+
+tela.listen()
+tela.onkeypress(barra_b_cima, "Up")
+
+tela.listen()
+tela.onkeypress(barra_b_baixo, "Down")
+
+# Main game loop
 while True:
     tela.update()
